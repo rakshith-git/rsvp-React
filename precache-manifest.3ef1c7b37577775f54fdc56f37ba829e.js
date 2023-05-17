450db249cb4eeefd248b39d0c262402fb19a450d
rakshith <62304358+rakshith-git@users.noreply.github.com>
2023-05-18 02:01:56 +0530

    Updates

---
+++ precache-manifest.3ef1c7b37577775f54fdc56f37ba829e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a205b70ce8ec5b8a9cde",
+    "url": "/rsvp-React/static/css/main.c7c5f053.chunk.css"
+  },
+  {
+    "revision": "a205b70ce8ec5b8a9cde",
+    "url": "/rsvp-React/static/js/main.17d7150c.chunk.js"
+  },
+  {
+    "revision": "2eb9c388b44244590239",
+    "url": "/rsvp-React/static/js/runtime~main.a19aa08d.js"
+  },
+  {
+    "revision": "f3afda117f246c896c6c",
+    "url": "/rsvp-React/static/js/2.ca48165b.chunk.js"
+  },
+  {
+    "revision": "ef276a8df7b31e3a5070",
+    "url": "/rsvp-React/static/js/3.d62f7fe7.chunk.js"
+  },
+  {
+    "revision": "99894da920ab86edbbacae823ce50889",
+    "url": "/rsvp-React/index.html"
+  }
+];
